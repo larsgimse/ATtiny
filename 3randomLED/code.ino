40ce8770e08ba677521290796aae201e7ee27ce6
Lars Gimse <larsgimse@gmail.com>
2019-01-23 22:09:00 +0100

    Create code.ino

---
+++ 3randomLED/code.ino
@@ -0,0 +1,14 @@
+int leds[3] = {0,1,2};
+
+void setup(){
+  for (int jj; jj<sizeof(leds)/sizeof(int);jj++){
+    pinMode(leds[jj],OUTPUT);
+    delay(10);
+  }
+}
+
+void loop(){
+  digitalWrite(leds[random(0,sizeof(leds)/sizeof(int))],HIGH);
+  delay(random(20,200));
+  digitalWrite(leds[random(0,sizeof(leds)/sizeof(int))],LOW);
+}
